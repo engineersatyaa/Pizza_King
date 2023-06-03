9cc77dc6a9264002a3939d6d144a52b1a60533e5
SATYA PAL SINGH <engineersatyaa@gmail.com>
2023-06-04 01:38:27 +0530

    s

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,13 @@
 import Wrapper from "@/components/Wrapper";
 import Link from "next/link";
+import { useState } from "react";
+import { ImSpinner6 } from "react-icons/im";
+import { IoMdEyeOff, IoMdEye } from "react-icons/io";
 import { IoHome } from "react-icons/io5";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div
       style={{ backgroundImage: "url('/slide1.jpg')" }}
@@ -31,13 +36,87 @@ function Login() {
 
           {/* Header block end */}
 
-          {/* Form block start */}
+          {/* form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
 
-          <div className="border w-1/2">
-            <form></form>
+    input {
+      border: 1px solid lightgray;
+      border-radius: 5px;
+      padding: 10px;
+      outline: none;
+      font-size: 15px;
+    } */}
+
+          {/* Login Form block start */}
+
+          <div className="border rounded-md border-white/10 w-[90%] p-3 sm:p-4  bg-white/10 shadow-[0px_0px_5px_8px_rgba(0,0,0,0.1)] text-black">
+            <div className="bg-white/80 p-3 sm:p-4 rounded-md ">
+              <form className="border flex flex-col gap-3 sm:gap-4">
+                <input
+                  type="text"
+                  placeholder="Phone or Email"
+                  className="border border-gray-400 rounded outline-none p-1 sm:p-2 text-[15px] min-h-[34px] "
+                />
+
+                <div className="flex items-center border border-gray-400 rounded overflow-hidden bg-white min-h-[34px]">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    className="w-full h-full outline-none text-[15px] p-1 sm:p-2"
+                  />
+
+                  <div
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="p-1 mx-1"
+                  >
+                    {showPassword ? <IoMdEye /> : <IoMdEyeOff />}
+                  </div>
+                </div>
+
+                {false && (
+                  <span className="text-center text-[15px] text-red-600">
+                    Error Messages
+                  </span>
+                )}
+
+                <button
+                  type="button"
+                  className="bg-black/95 text-white rounded sm:p-2 active:scale-95 transition-all duration-75 ease-linear flex items-center justify-center min-h-[34px] md:hover:bg-red-600 md:cursor-pointer"
+                >
+                  {false ? <ImSpinner6 className="animate-spin" /> : "Login"}
+                </button>
+              </form>
+
+              <div className=" border flex flex-col gap-3 sm:gap-4 mt-3 sm:mt-4">
+                <Link
+                  href=""
+                  className="text-center text-[15px] text-red-600 md:cursor-pointer"
+                >
+                  Forgotten Password ?
+                </Link>
+
+                <p className="text-center text-xs -mt-1">
+                  By logging in or creating an account, you agree with our
+                  <span className="text-red-600 mx-1">Terms & Conditions</span>
+                  and
+                  <span className="text-red-600 mx-1">Privacy Statement. </span>
+                </p>
+
+                <hr className="border-t border-t-black/20" />
+
+                <Link
+                  href="/register"
+                  className="bg-red-600 text-white rounded sm:p-2 active:scale-95 transition-all duration-75 ease-linear flex items-center  justify-center min-h-[34px] md:hover:bg-black/95 md:cursor-pointer"
+                >
+                  Create New Account
+                </Link>
+              </div>
+            </div>
           </div>
 
-          {/* Form block end */}
+          {/* Login Form block end */}
 
           {/* Footer block start */}
 
